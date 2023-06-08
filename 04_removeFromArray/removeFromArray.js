979a2311a198ef28a246cfdc009fc7cb7e0c46e3
dzmoney <115180206+dzmoney@users.noreply.github.com>
2023-06-08 16:49:40 -0400

    complete all but last test removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,15 +1,20 @@
-const removeFromArray = function (array, number) {
-  //find the index position of number you want to remove
-  const index = array.indexOf(number);
-  //if number exists in array (aka the above line does not return -1)
-  if (index !== -1) {
-    //remove 1 element from specified index of array
-    array.splice(index, 1);
-  }
-  return array;
+const removeFromArray = function (array, ...nums) {
+  //make shallow copy of array so it remains intact when we perform removal operations
+  const newArray = [...array];
+
+  nums.forEach((num) => {
+    //find the index position of number you want to remove
+    const index = newArray.indexOf(num);
+    //if number exists in array (aka the above line does not return -1)
+    if (index !== -1) {
+      //remove 1 element from specified index of array
+      newArray.splice(index, 1);
+    }
+  });
+  return newArray;
 };
 
-const numbers = [1, 2, 3, 4];
-const splicedArray = removeFromArray(numbers, 3);
+const numbers = [1, 2, 3];
+removeFromArray(numbers, "1", 3);
 // Do not edit below this line
 module.exports = removeFromArray;
