50554377e552741029c3c6108a2655057f900931
dzmoney <115180206+dzmoney@users.noreply.github.com>
2023-06-07 21:52:14 -0400

    Complete repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    let result = "";
+    for (s = 0; s < num; s++) {
+      result += string;
+    }
+    return result;
+  }
 };
+
+repeatString("", 5);
 
 // Do not edit below this line
 module.exports = repeatString;
